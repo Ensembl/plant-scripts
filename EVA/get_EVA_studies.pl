e513aff656128f870108c72431c1b2dbeac2b916
brunocontrerasmoreira <bcontreras@ebi.ac.uk>
2018-11-16 11:23:52 +0000

    now takes GCA accession

--- EVA/get_EVA_studies.pl
+++ EVA/get_EVA_studies.pl
@@ -9,11 +9,21 @@ use JSON qw(from_json);
 
 my $EVASTUDYLIST = 'https://www.ebi.ac.uk/eva/webservices/rest/v1/meta/studies/all';
 
+my $GCA_acc;
+if(!$ARGV[0]){ 
+	die "# usage: $0 <GCA accession>\n\n";
+} else {
+	$GCA_acc = $ARGV[0];
+}
+
 my $cl=REST::Client->new(); 
 $cl->GET($EVASTUDYLIST,{Accept => 'application/json'}); 
 my $json = from_json($cl->responseContent());
 
 foreach my $study (@{ $json->{'response'}->[0]->{'result'} }){
+
+	next if($study->{'assemblyAccession'} !~ /$GCA_acc/);
+
 	print "$study->{'id'}\t$study->{'assemblyAccession'}\t$study->{'name'}\n";
 }
 

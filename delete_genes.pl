886053a469981195e73cecea9ef598189bcce97e
brunocontrerasmoreira <bcontreras@ebi.ac.uk>
2018-12-20 15:12:18 +0000

    added -help and counter for deleted genes

--- delete_genes.pl
+++ delete_genes.pl
@@ -2,14 +2,16 @@
 use strict;
 use warnings;
 
-# from https://www.ebi.ac.uk/seqdb/confluence/display/EnsGen/Core+database+production#Coredatabaseproduction-Reusinganassembly
+# Removes genes (or any user-selected logic names/biotypes) from a core db; adapted from 
+# https://www.ebi.ac.uk/seqdb/confluence/display/EnsGen/Core+database+production
 
 use Getopt::Long qw(:config no_ignore_case);
 use Bio::EnsEMBL::DBSQL::DBAdaptor;
  
-my ($host, $port, $user, $pass, $dbname, @logic_name, @biotype);
+my ($help, $host, $port, $user, $pass, $dbname, @logic_name, @biotype);
  
 GetOptions(
+  "help|?" => \$help,
   "host=s", \$host,
   "P|port=i", \$port,
   "user=s", \$user,
@@ -17,7 +19,21 @@ GetOptions(
   "dbname=s", \$dbname,
   "logic_name=s", \@logic_name,
   "biotype=s", \@biotype,
-);
+) || help_message(); 
+
+if($help){ help_message() }
+
+sub help_message {
+	print "\nusage: $0 [options]\n\n".
+	"-host                 (required)\n".
+	"-port                 (required)\n".
+	"-user                 (required)\n".
+	"-pass                 (required)\n".
+	"-dbname               (required)\n".
+	"-biotype              (optional, example: -biotype protein_coding)\n".
+	"-logic_names          (optional, example: -logic_names maker)\n";
+	exit(0);
+}
  
 my $dba = Bio::EnsEMBL::DBSQL::DBAdaptor->new
 (
@@ -31,12 +47,18 @@ my $dba = Bio::EnsEMBL::DBSQL::DBAdaptor->new
 my $ga = $dba->get_adaptor("Gene");
  
 my %biotype = map { $_ => 1 } @biotype;
- 
+
+my $deleted = 0;
+
 foreach my $logic_name (@logic_name) {
   my $genes = $ga->fetch_all_by_logic_name($logic_name);
   foreach my $gene (@$genes) {
     if (scalar(@biotype) == 0 || exists $biotype{$gene->biotype}) {
       $ga->remove($gene);
+      $deleted++;
     }
   }
 }
+
+print "# deleted $deleted genes\n\n";
+

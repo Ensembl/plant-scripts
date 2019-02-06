002e9eca9da15556cc2a8b0f3ed1df119e39166a
brunocontrerasmoreira <bcontreras@ebi.ac.uk>
2019-02-06 10:58:37 +0000

    added -f to allow parsing a TSV file, as those made with check_new_ENA_assemblies.pl

--- EVA/get_EVA_studies.pl
+++ EVA/get_EVA_studies.pl
@@ -1,19 +1,43 @@
 #!/usr/bin/env perl
 # gets available EVA studies for a user-provided GCA_accession
-# 2018 Bruno Contreras Moreira EMBL-EBI
+# 2018-9 Bruno Contreras Moreira EMBL-EBI
 
 use strict;
 use warnings;
+use Getopt::Std;
 use REST::Client;
 use JSON qw(from_json);
 
 my $EVASTUDYLIST = 'https://www.ebi.ac.uk/eva/webservices/rest/v1/meta/studies/all';
 
-my $GCA_acc;
-if(!$ARGV[0]){ 
-	die "# usage: $0 <GCA accession>\n\n";
-} else {
-	$GCA_acc = $ARGV[0];
+my ($GCA_acc,$TSV_file,%wantedGCA,%opts);
+
+getopts('ha:f:', \%opts);
+
+if(($opts{'h'})||(scalar(keys(%opts))==0)){
+  print "\nusage: $0 [options]\n\n";
+  print "-h this message\n";
+  print "-a GCA accession                              (example: -a GCA_000001735.2 )\n";
+  print "-f TSV file with GCA accessions in 1st column (example: -f Viridiplantae.2019-01-22.report.tsv)\n\n";
+  exit(0);
+}
+
+if($opts{'a'}){ 
+	$GCA_acc = $opts{'a'};
+	$wantedGCA{$GCA_acc} = 1;
+}
+elsif($opts{'f'}){
+	$TSV_file = $opts{'f'};
+	open(TSV,"<",$TSV_file) || die "# ERROR: cannot read $TSV_file\n";
+	while(<TSV>) {
+		next if(/^#/);
+		$GCA_acc = (split(/\t/,$_))[0];
+		$wantedGCA{$GCA_acc} = 1;
+	}
+	close(TSV);
+}
+else {
+	die "# ERROR: please use -a OR -f\n\n";
 }
 
 my $cl=REST::Client->new(); 
@@ -22,7 +46,9 @@ my $json = from_json($cl->responseContent());
 
 foreach my $study (@{ $json->{'response'}->[0]->{'result'} }){
 
-	next if($study->{'assemblyAccession'} !~ /$GCA_acc/);
+	$GCA_acc = $study->{'assemblyAccession'};
+
+	next if(!$wantedGCA{ $GCA_acc });
 
 	print "$study->{'id'}\t$study->{'assemblyAccession'}\t$study->{'name'}\n";
 }

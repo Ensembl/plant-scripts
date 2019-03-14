69edf90f4840259ba545666a289573526f03d526
brunocontrerasmoreira <bcontreras@ebi.ac.uk>
2019-03-14 10:41:55 +0000

    commented git clone from fork

--- core/load_genome_shell.pl
+++ core/load_genome_shell.pl
@@ -97,6 +97,8 @@ if($thishost =~ m/ebi-login/ || $thishost =~ m/ebi-cli/){
 # NOTE: you'll need to add your public key to the list if github SSH keys
 if(!-d $GCA_accession){
 	system("git clone git\@github.com:Ensembl/ensembl-genomeloader.git $GCA_accession");
+	#testing fork
+	#system("git clone git\@github.com:brunocontrerasmoreira/ensembl-genomeloader.git $GCA_accession");
 }
 else {
     chdir($GCA_accession);

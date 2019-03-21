733d486ecb81dca4bb141cf8c1cae5d8084cb579
brunocontrerasmoreira <bcontreras@ebi.ac.uk>
2019-03-21 10:50:16 +0000

    updated hive server

--- core/allxref_pipeline_hive.pl
+++ core/allxref_pipeline_hive.pl
@@ -20,7 +20,7 @@ use Bio::EnsEMBL::Registry;
 ## check user arguments ######################################################
 ##############################################################################
 
-my $hive_db_cmd = 'mysql-eg-hive-ensrw';
+my $hive_db_cmd = 'mysql-ens-hive-prod-2-ensrw';
 my ($rerun,$overwrite) = (0,0);
 my ($help,$reg_file,@species,$species_cmd,$ensembl_version,$pipeline_dir);
 my ($hive_args,$hive_url,$hive_db);      

95367f5e43ea4e1f755c6675cbac0d5b8e4c6eb8
brunocontrerasmoreira <bcontreras@ebi.ac.uk>
2019-09-18 11:33:24 +0100

    use ComparaUtils

--- compara/ortholog_annotation.pl
+++ compara/ortholog_annotation.pl
@@ -6,6 +6,12 @@ use Getopt::Long qw(:config no_ignore_case);
 use JSON qw(decode_json);
 use Time::HiRes;
 use HTTP::Tiny;
+use FindBin '$Bin';
+use lib $Bin;
+use ComparaUtils qw(
+	perform_rest_action  
+   $REQUEST_COUNT 
+);
 
 # Retrieves annotations for an Ensembl gene/protein transfered from its orthologues 
 # Bruno Contreras Moreira 2019
@@ -40,14 +46,15 @@ if($id eq ''){
 
 print "# $0 -n $nondirect -i $id\n\n";
 
-# new object for REST requests
+# new object and params for REST requests
 my $http = HTTP::Tiny->new();
 my $global_headers = { 'Content-Type' => 'application/json' };
-my $request_count = 0; # global counter to avoid overload
+$ComparaUtils::REQUEST_COUNT = 0;
+
 
 # get orthologues & their annotations
 $request = $HOMOLPOINT."$id?type=orthologues";
-$response = perform_rest_action( $request, $global_headers );
+$response = perform_rest_action( $http, $request, $global_headers );
 $dump = decode_json($response);
 @orths = @{ $dump->{'data'}->[0]->{'homologies'} };
 if(scalar(@orths == 0)){
@@ -61,7 +68,7 @@ foreach $orth (@orths) {
 	
 	# check annotations of this ortholog
 	$request = $XREFPOINT."id/$orth_id?";
-	$response = perform_rest_action( $request, $global_headers );
+	$response = perform_rest_action( $http, $request, $global_headers );
 	my $xref_dump = decode_json($response);
 	
 	foreach $xref (@{ $xref_dump }) {
@@ -98,48 +105,5 @@ else{
 			}
 		}
 	}
-}
-
-# uses global $request_count
-# based on https://github.com/Ensembl/ensembl-rest/wiki/Example-Perl-Client
-sub perform_rest_action {
-	my ($url, $headers) = @_;
-	$headers ||= {};
-	$headers->{'Content-Type'} = 'application/json' unless exists $headers->{'Content-Type'};
-
-	if($request_count == 15) { # check every 15
-		my $current_time = Time::HiRes::time();
-		my $diff = $current_time - $last_request_time;
-
-		# if less than a second then sleep for the remainder of the second
-		if($diff < 1) {
-			Time::HiRes::sleep(1-$diff);
-		}
-		# reset
-		$last_request_time = Time::HiRes::time();
-		$request_count = 0;
-	}
-
-	my $response = $http->get($url, {headers => $headers});
-	my $status = $response->{status};
-	
-	if(!$response->{success}) {
-		# check for rate limit exceeded & Retry-After (lowercase due to our client)
-		if(($status == 429 || $status == 599) && exists $response->{headers}->{'retry-after'}) {
-			my $retry = $response->{headers}->{'retry-after'};
-			Time::HiRes::sleep($retry);
-			# afterr sleeping see that we re-request
-			return perform_rest_action($url, $headers);
-		}
-		else {
-			my ($status, $reason) = ($response->{status}, $response->{reason});
-			die "# ERROR: failed REST request $url\n# Status code: ${status}\n# Reason: ${reason}\n# Please re-run";
-		}
-	}
-
-	$request_count++;
-
-	if(length($response->{content})) { return $response->{content} } 
-	else { return '' }	
 }
 

b479753dc5211e928b12c458e7bce75324fb71f0
brunocontrerasmoreira <bcontreras@ebi.ac.uk>
2019-10-01 15:02:18 +0100

    not imports transverse_tree_json from ComparaUtils.pm

--- compara/ens_synthelogs.pl
+++ compara/ens_synthelogs.pl
@@ -10,13 +10,12 @@ use JSON qw(decode_json);
 use FindBin '$Bin';
 use lib $Bin;
 use ComparaUtils qw(
-	download_compara_TSV_file perform_rest_action  
+	download_compara_TSV_file perform_rest_action transverse_tree_json
    $REQUEST_COUNT $COMPARADIR @DIVISIONS 
 );
 
-# Retrieves single-copy orthologous genes/proteins shared by (plant) species in clade 
+# Retrieves orthologous, synthenic genes (synthelogs) shared by (plant) species in clade
 # by querying pre-computed Compara data from Ensembl Genomes with a reference genome.
-# Multiple copies are optionally allowed for selected or all species.
 #
 # Bruno Contreras Moreira 2019
 
@@ -30,7 +29,7 @@ my $downloadir = $Bin . '/downloads';
 my $verbose    = 0;
 my $division   = 'Plants';
 my $taxonid    = ''; # NCBI Taxonomy id, Brassicaceae=3700, Asterids=71274, Poaceae=4479
-my $ref_genome = 'arabidopsis_thaliana'; # should be diploid and contained in $taxonid;
+my $ref_genome = ''; # should be diploid and contained in $taxonid;
 my $seqtype    = 'protein'; 
 my $comparadir = '';
 my $outfolder  = '';
@@ -38,8 +37,8 @@ my $out_genome = '';
 
 
 my ($help,$sp,$show_supported,$request,$response);
-my ($GOC,$WGA,$one2many,$LOWCONF) = (0,0,0,0);
-my (@multi_species, @ignore_species, %ignore, %polyploid, %division_supported);
+my ($GOC,$LOWCONF) = (75,0);
+my (@multi_species, @ignore_species, %ignore, %division_supported);
 
 GetOptions(	
 	"help|?"       => \$help,
@@ -49,11 +48,9 @@ GetOptions(
 	"clade|c=s"    => \$taxonid,
 	"reference|r=s"=> \$ref_genome,
 	"outgroup|o=s" => \$out_genome,
-	"multicopy|m=s"=> \@multi_species,
 	"ignore|i=s"   => \@ignore_species,
 	"type|t=s"     => \$seqtype,
 	"GOC|G=i"      => \$GOC,
-	"WGA|W=i"      => \$WGA,
 	"LC|L"         => \$LOWCONF,
 	"folder|f=s"   => \$outfolder
 ) || help_message(); 
@@ -65,7 +62,6 @@ sub help_message {
 		"-d Ensembl division                     (optional, default: -d $division)\n".
 		"-r reference species_name               (optional, default: -r $ref_genome)\n".
 		"-o outgroup species_name                (optional, example: -o brachypodium_distachyon)\n".
-		"-m multi-copy species_name(s)           (optional, example: -m brassica_napus -m ... -m all)\n".
 		"-i ignore species_name(s)               (optional, example: -i selaginella_moellendorffii -i ...)\n".
 		"-f folder to output FASTA files         (optional, example: -f myfolder)\n".
 		"-t sequence type [protein|cdna]         (optional, requires -f, default: -t protein)\n".
@@ -73,20 +69,15 @@ sub help_message {
 		"-v verbose                              (optional, example: -v\n";
 
 	print "\nThe following options are only available for some clades:\n\n".
-		"-G min Gene Order Conservation [0:100]  (optional, example: -G 75)\n".
+		"-G min Gene Order Conservation [1:100]  (optional, default: -G $GOC)\n".
 		"   see modules/Bio/EnsEMBL/Compara/PipeConfig/EBI/Plants/ProteinTrees_conf.pm\n".
 		"   at https://github.com/Ensembl/ensembl-compara\n\n".
-		"-W min Whole Genome Align score [0:100] (optional, example: -W 75)\n".
-		"   see ensembl-compara/scripts/pipeline/compara_plants.xml\n".
-		"   at https://github.com/Ensembl/ensembl-compara\n\n";
-	print "Read about GOC and WGA at:\n".
+	print "Read about GOC at:\n".
 		"https://www.ensembl.org/info/genome/compara/Ortholog_qc_manual.html\n\n";
 
 	print "Example calls:\n\n".
 		" perl $0 -c Brassicaceae -f Brassicaceae\n".
-		" perl $0 -c Brassicaceae -f Brassicaceae -t cdna -o theobroma_cacao\n".
-		" perl $0 -f poaceae -c 4479 -r oryza_sativa -WGA 75\n".
-		" perl $0 -f all -c 33090 -m all -r physcomitrella_patens\n";
+		" perl $0 -c Brassicaceae -f Brassicaceae -t cdna -o theobroma_cacao\n";
 		exit(0);
 }
 
@@ -114,29 +105,21 @@ else{
    	$taxonid =~ s/\s+/%20/g;
 	}
 
+   if($ref_genome eq ''){
+   	print "# ERROR: need a valid reference species_name, such as -r arabidopsis_thaliana)\n\n";
+   	exit;
+	} 
+
+	if($GOC < 1){
+		print "# ERROR: please set a GOC value within [1,100]\n\n";
+      exit;
+	}
+
 	if(@ignore_species){
 		foreach my $sp (@ignore_species){
 			$ignore{ $sp } = 1;
 		}
 		printf("\n# ignored species : %d\n\n",scalar(keys(%ignore)));
-	}
-
-	# species for which one2many orths are allowed, such as polyploid species
-	# with scaffold level assemblies or species with ancestral genome duplications
-	if(@multi_species){
-		foreach my $sp (@multi_species){
-			if($sp eq 'all'){
-				$one2many = 1;
-				%polyploid = ();
-				$polyploid{ $sp } = 1;
-				last;
-			} else{ $polyploid{ $sp } = 1 }
-		}
-
-		if($one2many){ print "\n# multi-copy species : all\n\n" }
-		else{
-			printf("\n# multi-copy species : %d\n\n",scalar(keys(%polyploid)));
-		}
 	}
 
 	if($outfolder){
@@ -151,7 +134,7 @@ else{
 	}	
 
 	print "# $0 -d $division -c $taxonid -r $ref_genome -o $out_genome ".
-		"-f $outfolder -t $seqtype -G $GOC -W $WGA -L $LOWCONF\n\n";
+		"-f $outfolder -t $seqtype -G $GOC -L $LOWCONF\n\n";
 }
 
 my $start_time = new Benchmark();
@@ -211,12 +194,9 @@ foreach $sp (@{ $infodump }) {
 
 printf("# supported species in NCBI taxon %s : %d\n\n", $taxonid, scalar(@supported_species));
 
-# check reference genome is supported and is not polyploid
+# check reference genome is supported
 if(!grep(/$ref_genome/,@supported_species)){
 	die "# ERROR: cannot find $ref_genome within NCBI taxon $taxonid\n";
-}
-elsif($polyploid{ $ref_genome }){
-	   die "# ERROR: $ref_genome is polyploid; reference genome must be diploid\n";
 }
 
 # add outgroup if required
@@ -229,7 +209,7 @@ if($out_genome){
 $n_of_species = scalar( @supported_species );
 print "# total selected species : $n_of_species\n\n";
 
-## 2) get orthologous (plant) genes shared by $ref_genome and other species ####################
+## 2) get (plant) synthelogous genes shared by $ref_genome and other species ####################
 
 # columns of TSV file 
 my ($gene_stable_id,$prot_stable_id,$species,$identity,$homology_type,$hom_gene_stable_id,
@@ -255,16 +235,13 @@ while(<TSV>){
 
 	next if(	!$supported{ $hom_species } || $hom_species eq $ref_genome); 
 
-	if($high_confidence){
+	if(defined($high_confidence)){
 		next if($LOWCONF == 0 && ($high_confidence eq 'NULL' || $high_confidence == 0));
 	}
 
-	next if($WGA && ($wga_coverage eq 'NULL' || $wga_coverage < $WGA));
+	next if($goc_score eq 'NULL' || $goc_score < $GOC));
 
-	next if($GOC && ($goc_score eq 'NULL' || $goc_score < $GOC));
-
-	if($homology_type eq 'ortholog_one2one' || 
-		(($one2many || $polyploid{ $hom_species } ) && $homology_type eq 'ortholog_one2many') ) {
+	if($homology_type eq 'ortholog_one2one' || $homology_type eq 'ortholog_one2many') {
 
 		# add $ref_genome protein 
 		if(!$core{ $gene_stable_id }){ 
@@ -283,17 +260,15 @@ while(<TSV>){
 }
 close(TSV);
 
-# check GOC / WGA availability
+# check GOC availability
 foreach $hom_species (@supported_species){
-	if(!defined( $present{ $hom_species } )&& $WGA){
-		print "# WGA not available: $hom_species\n";
-	} elsif(!defined( $present{ $hom_species } ) && $GOC){
+	if(!defined( $present{ $hom_species } ) && $GOC){
 		print "# GOC not available: $hom_species\n";
 	}
 }
 
 
-## 3) print summary matrix of single-copy / core genes and compile sequence clusters #################
+## 3) print summary matrix of synthelogs and compile sequence clusters #################
 
 my $total_core_clusters = 0;
 my ($pruned_species,$treedump,$acc,$seq,$line,$filename);
@@ -392,7 +367,7 @@ foreach $gene_stable_id (@sorted_ids){
 	$total_core_clusters++; #last if($total_core_clusters == 1); # debug
 }
 
-print "\n# total single-copy core clusters : $total_core_clusters\n\n";
+print "\n# total synthelog clusters : $total_core_clusters\n\n";
 
 # print diagnostics
 if($total_core_clusters == 0){
@@ -408,24 +383,3 @@ if($total_core_clusters == 0){
 
 my $end_time = new Benchmark();
 print "\n# runtime: ".timestr(timediff($end_time,$start_time),'all')."\n";
-
-##############################################################################
-
-# takes decoded JSON->{'tree'} returned from $TREEPOINT and fills a hash with
-# accession, sequence pairs
-sub transverse_tree_json {
-
-   my ($tree, $ref_hash) = @_;
-
-   my ($child,$id);
-
-   if($tree->{'sequence'}){
-      foreach $id (@{ $tree->{'sequence'}{'id'} }){
-         $ref_hash->{ $id->{'accession'} } = $tree->{'sequence'}{'mol_seq'}{'seq'};
-      }
-   }
-
-   foreach $child (@{ $tree->{'children'} }){
-      transverse_tree_json( $child, $ref_hash );
-   }
-}

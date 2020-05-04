1e7ec7d7ba863b99c4f0457aa48898d175db0f80
brunocontrerasmoreira <bcontreras@ebi.ac.uk>
2020-05-04 17:26:40 +0100

    pep FASTA works

--- book_chapter_code/exampleFTP.sh
+++ book_chapter_code/exampleFTP.sh
@@ -1,30 +1,33 @@
 
-# set FTP server
-SERVER=ftp://ftp.ensemblgenomes.org/pub/plants
+# set FTP server & division
+SERVER=ftp://ftp.ensemblgenomes.org/pub
+DIV=plants
 
-# set Ensembl and Ensembl Genomes release numbers,
-# check them out at the bottom of the Web sites
-RELEASE=100       # http://www.ensembl.org
-EGRELEASE=47      # http://plants.ensembl.org
+# set Ensembl Plants release number, check it 
+# at the bottom of http://plants.ensembl.org
+EGRELEASE=47      
 
-# Ensembl Plants FTP server can be found at
-# ftp://ftp.ensemblgenomes.org/pub/plants/current
-URL=${SERVER}/current
+RELEASE=$(( $EGRELEASE + 53)); # do not change
 
-# you could also choose a specific version
-#URL=${SERVER}/release-${EGRELEASE}
+# set example species
+SPECIES=Brachypodium_distachyon
 
-SPECIES=brachypodium_distachyon
+echo "EGRELEASE=${EGRELEASE}"
+echo
 
 # 1) download peptide sequences in FASTA format
 
+FASTAPEP=${SPECIES}*pep.all.fa.gz
+wget -c ${SERVER}/release-${EGRELEASE}/${DIV}/fasta/${SPECIES,,}/pep/${FASTAPEP} .
 
 # 2) download transcripts in FASTA format
 
+
+
 # 3) download raw and repeat-masked genomic sequence
 
-# x) download all homologies in a single TSV file
-# See ${URL}/tsv/ensembl-compara/homologies/README.gene_trees.tsv_dumps.txt)
+# x) download all homologies in a single TSV file, several GBs
+
 TSVFILE=Compara.${RELEASE}.protein_default.homologies.tsv.gz
-wget -c ${URL}/tsv/ensembl-compara/homologies/${TSVFILE} .
+wget -c ${SERVER}/${DIV}/release-${EGRELEASE}/tsv/ensembl-compara/homologies/${TSVFILE} .
 

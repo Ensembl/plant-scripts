1041454be96d56a122eb0fd459d3757104f4fce5
brunocontrerasmoreira <bcontreras@ebi.ac.uk>
2020-05-04 16:37:23 +0100

    does not work

---
+++ book_chapter_code/2017/examplePerlAPI.pl
@@ -0,0 +1,91 @@
+# Example adapted from https://europepmc.org/article/MED/27987162
+
+# 1. Install the Ensembl Perl API
+
+# 2. Load the Registry object with details of genomes available
+# from the public Ensembl Genomes servers:
+use warnings;
+use strict;
+use Bio::EnsEMBL::Registry;
+
+Bio::EnsEMBL::Registry->load_registry_from_db(
+	-host => 'mysql-eg-publicsql.ebi.ac.uk',
+	-port => '4157',
+);
+
+# 3. Find the DEAR3 gene from A. thaliana :
+
+# gene to look for
+my $gene_name = 'DEAR3';
+
+# species to look for
+my $species = 'arabidopsis_thaliana';
+
+exit;
+
+# get a gene adaptor to work with genes from
+# the species
+my $gene_adaptor = Bio::EnsEMBL::Registry->
+	get_adaptor($species, 'core', 'gene');
+
+exit;
+
+# find the gene with the specifi ed name using
+# the adaptor
+my $gene_obj = @{$gene_adaptor->
+	fetch_all_by_external_name($gene_name)};
+
+# 4. Find all orthologues from Tracheophytes in the plant Compara:
+
+# compara database to search in
+my $division = 'plants';
+
+exit;
+
+# get an adaptor to work with genes from compara
+my $gene_member_adaptor = Bio::EnsEMBL::Registry->
+	get_adaptor($division, 'compara', 'GeneMember');
+
+# find the corresponding gene in compara
+my $gene_member = $gene_member_adaptor->
+	fetch_by_source_stable_id('ENSEMBLGENE',
+		$gene_obj->stable_id);
+
+# get an adaptor to work with homologues in compara
+my $homology_adaptor = Bio::EnsEMBL::Registry->
+	get_adaptor($division, 'compara', 'Homology');
+
+# find all homologues of the gene
+my @homologies = @{$homology_adaptor->
+	fetch_all_by_Member($gene_member)};
+
+# filter out homologues based on taxonomy and type
+@homologies = grep {
+	$_->taxonomy_level eq 'Tracheophyta' &&
+	$_->description =~ m/ortholog/
+} @homologies;
+
+# 5. Find each orthologous protein:
+foreach my $homology (@homologies) {
+
+	# get the protein from the target
+	my $target = $homology->get_all_Members->[1];
+	
+	my $translation = $target->get_Translation;
+	
+	print $target->genome_db->name, ' orthologue ',
+		$translation->stable_id, "\n";
+	
+# 6. For each translation, print information about GO annotation:
+
+	# find all the GO terms for this translation
+	foreach my $go_term (@{$translation->get_all_DBEntries('GO')}) {
+	
+	# print some information about each GO annotation
+	print $go_term->primary_id, ' ', $go_term->description;
+	
+	# print the evidence for the GO annotation
+	print ' Evidence: ', (join ', ', map {$_->[0]}
+		@{$go_term->get_all_linkage_info}), "\n";
+	}
+}

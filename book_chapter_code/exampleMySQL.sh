e00a64d1d429d475406e38ca75fa95d7f99ec6f8
brunocontrerasmoreira <bcontreras@ebi.ac.uk>
2020-05-04 17:58:28 +0100

    finds species core

--- book_chapter_code/exampleMySQL.sh
+++ book_chapter_code/exampleMySQL.sh
@@ -2,6 +2,30 @@
 # documentation about Ensembl schemas can be found at 
 # http://www.ensembl.org/info/docs/api/index.html
 
-mysql --host mysql-eg-publicsql.ebi.ac.uk --user anonymous \
-	--port 4157 arabidopsis_thaliana_core_47_100_11 \
-	-e "select * from gene"
+# set server details
+SERVER=mysql-eg-publicsql.ebi.ac.uk
+USER=anonymous
+PORT=4157
+
+# set Ensembl Plants release number, check it
+# at the bottom of http://plants.ensembl.org
+EGRELEASE=47
+
+RELEASE=$(( $EGRELEASE + 53)); # do not change
+
+# set example species
+SPECIES=arabidopsis_thaliana
+
+echo "EGRELEASE=${EGRELEASE}"
+echo
+
+
+# 1) check currently supported species and schemas
+
+SPECIESCORE=`mysql --host $SERVER --user $USER --port $PORT \
+	-e "show databases" | grep "${SPECIES}_core_${EGRELEASE}_${RELEASE}"`
+
+# 2) count protein-coding genes
+
+mysql --host $SERVER --user $USER --port $PORT \
+	$SPECIESCORE -e "SELECT COUNT(*) FROM gene WHERE biotype='protein_coding'"

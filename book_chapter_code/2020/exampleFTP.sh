1dd05d47432a9fc2352cd8695cd1faf124b0e83f
brunocontrerasmoreira <bcontreras@ebi.ac.uk>
2020-05-04 13:26:15 +0100

    draft

---
+++ book_chapter_code/2020/exampleFTP.sh
@@ -0,0 +1,14 @@
+
+# Ensembl Plants FTP server can be found at 
+# ftp://ftp.ensemblgenomes.org/pub/plants/current
+
+# you can also choose a previous version, ie
+# ftp://ftp.ensemblgenomes.org/pub/plants/release-44
+
+# 1) download peptide sequences in FASTA format
+
+# 2) download transcripts in FASTA format
+
+# 3) download raw and repeat-masked genomic sequence
+
+

32167d95213d58a229e248868630b0cab53b4ccc
brunocontrerasmoreira <bcontreras@ebi.ac.uk>
2020-05-06 16:28:03 +0100

    added overlap examples

--- demo_user_scripts/exampleREST.pl
+++ demo_user_scripts/exampleREST.pl
@@ -18,8 +18,8 @@ use JSON;
 use HTTP::Tiny;
 use Data::Dumper;
 
-# 1. Create an HTTP client and a helper function for invoking a
-# REST endpoint:
+## 1. Create an HTTP client and a helper function for invoking a
+##  REST endpoint:
 
 # create an HTTP client
 my $http = HTTP::Tiny->new;
@@ -38,27 +38,74 @@ sub call_endpoint {
 	return decode_json($response->{content});
 }
 
-# 2. Find homologues of A. thaliana DEAR3 gene:
+## 2. Find features overlapping genomic region, full list at
+## https://rest.ensembl.org/documentation/info/overlap_region
+
+my $species = 'triticum_aestivum';
+my $region = '3D:379400000-379540000';
+
+# genes
+my $url =
+    join('/', $server, 'overlap/region', $species, $region).
+	"?feature=gene;content-type=application/json";
+
+print "# check it on the browser: $url\n\n";
+
+# call url endpoint and get a hash back
+my $overlap_data = call_endpoint($url);
+
+# parse the data from the response
+foreach my $overlap_feat (@$overlap_data){
+	printf("%s\t%s\t%s\n",$overlap_feat->{id},
+		$overlap_feat->{start}, $overlap_feat->{end});
+}
+
+# now LTR repeats 
+$url = join('/', $server, 'overlap/region', $species, $region).
+		"?feature=repeat;content-type=application/json";
+
+$overlap_data = call_endpoint($url);
+
+foreach my $overlap_feat (@$overlap_data){
+
+	next if($overlap_feat->{description} !~ 'LTR');
+
+	printf("%s\t%s\t%s\n", $overlap_feat->{description},
+		$overlap_feat->{start}, $overlap_feat->{end});
+}
+
+# finally get EMS variants
+$url = join('/', $server, 'overlap/region', $species, $region).
+	"?feature=variation;content-type=application/json";
+
+$overlap_data = call_endpoint($url);
+
+foreach my $overlap_feat (@$overlap_data){
+	next if($overlap_feat->{source} ne 'EMS-induced mutation');
+	print $overlap_feat->{id}."\n";
+}
+
+## 3. Find homologues of A. thaliana DEAR3 gene
+$species = 'arabidopsis_thaliana';
 my $gene = 'DEAR3';
-my $species = 'arabidopsis_thaliana';
 my $division = 'plants';
 my $homoltype = 'ortholog';
 
-# Check https://www.ncbi.nlm.nih.gov/taxonomy for taxonomy ids
-my $target_taxon = 4479; #Poaceae
+# optional define a target clade, such as 4479
+# for Poaceae, see https://www.ncbi.nlm.nih.gov/taxonomy
+my $target_clade = 4479; 
 
-my $url =
+$url = 
 	join('/', $server, 'homology/symbol', $species, $gene).
 	"?content-type=application/json&compara=$division";
 
-if(defined($target_taxon)){
-	$url .= "&target_taxon=$target_taxon";
+# restrict to homologues from this clade/taxon
+if(defined($target_clade)){
+	$url .= "&target_taxon=$target_clade";
 }
 
-# call url endpoint and get a hash back
 my $homology_data = call_endpoint($url);
 
-# parse the homologue list from the response
 my @homologies = @{$homology_data->{data}[0]{homologies}};
 
 # filter out homologues based on type
@@ -68,7 +115,7 @@ if(defined($homoltype)){
 	} @homologies;
 }
 
-# 3. Print some information about the orthologous protein:
+## 4. Print some information about the orthologous protein
 for my $homolog (@homologies) {
 
 	my $target_species = $homolog->{target}{species};

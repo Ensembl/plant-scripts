5340e012ea69ce9677c3829d434195eae02a0c6f
brunocontrerasmoreira <bcontreras@ebi.ac.uk>
2020-05-06 17:45:58 +0100

    added wheat homoeologues

--- demo_user_scripts/exampleMySQL.sh
+++ demo_user_scripts/exampleMySQL.sh
@@ -19,25 +19,47 @@ EGRELEASE=47
 
 RELEASE=$(( $EGRELEASE + 53)); # do not change
 
-# set example species
-SPECIES=arabidopsis_thaliana
-
 echo "EGRELEASE=${EGRELEASE}"
 echo
 
-
-# 1) check currently supported Ensembl Genomes (EG) core schemas,
-# note it includes non-plants as well
+## 1) check currently supported Ensembl Genomes (EG) core schemas,
+##  note it includes non-plants as well
 
 mysql --host $SERVER --user $USER --port $PORT \
 	-e "show databases" | grep "core_${EGRELEASE}_${RELEASE}"
 
-# 2) count protein-coding genes of a particular species
+## 2) count protein-coding genes of a particular species
 
+SPECIES=arabidopsis_thaliana
 SPECIESCORE=`mysql --host $SERVER --user $USER --port $PORT \
     -e "show databases" | grep "${SPECIES}_core_${EGRELEASE}_${RELEASE}"`
 
 mysql --host $SERVER --user $USER --port $PORT \
 	$SPECIESCORE -e "SELECT COUNT(*) FROM gene WHERE biotype='protein_coding'"
+
+## 3) get Triticum aestivum homeologous genes across A,B & D subgenomes
+## Compara schema is described at 
+## https://m.ensembl.org/info/docs/api/compara/compara_schema.html
+
+# find out correct method_link_species_set.name
+mysql --host $SERVER --user $USER --port $PORT \
+	ensembl_compara_plants_${EGRELEASE}_$RELEASE -e \
+	"SELECT name from method_link_species_set" | \
+	grep "T.aes" | grep homoeologues
+
+# remove LIMIT 10 if you want the complete set
+mysql --host $SERVER --user $USER --port $PORT \
+    ensembl_compara_plants_${EGRELEASE}_$RELEASE <<SQL
+SELECT 
+	homology_member.homology_id, cigar_line, perc_cov, perc_id, 
+	perc_pos, gene_member.stable_id as genes, gene_member.genome_db_id
+FROM
+	homology_member 
+	INNER JOIN homology USING (homology_id)
+	INNER JOIN method_link_species_set USING (method_link_species_set_id)
+	INNER JOIN gene_member USING (gene_member_id)
+WHERE method_link_species_set.name="T.aes homoeologues" LIMIT 10
+SQL
+
 
 

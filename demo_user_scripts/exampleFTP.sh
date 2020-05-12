1e4fe4326ed37c6de1392235c88b1d8b5a3d80fc
eead-csic-compbio <bcontreras@eead.csic.es>
2020-05-12 12:57:32 +0100

    numbered recipes

--- demo_user_scripts/exampleFTP.sh
+++ demo_user_scripts/exampleFTP.sh
@@ -23,43 +23,44 @@ SPECIES=Brachypodium_distachyon
 echo "EGRELEASE=${EGRELEASE} OPTARG=$OPTARG"
 echo
 
-# 1) download peptide sequences in FASTA format
+## F1) download peptide sequences in FASTA format
 
 FASTAPEP=${SPECIES}*pep.all.fa.gz
 URL=${SERVER}/release-${EGRELEASE}/${DIV}/fasta/${SPECIES,,}/pep/${FASTAPEP}
 echo "# downloading $URL"
 wget $OPTARG -c $URL 
 
-# 2) download CDS nucleotide sequences in FASTA format
+## F2) download CDS nucleotide sequences in FASTA format
 
 FASTACDS=${SPECIES}*cds.all.fa.gz
 URL=${SERVER}/release-${EGRELEASE}/${DIV}/fasta/${SPECIES,,}/cds/${FASTACDS}
 echo "# downloading $URL"
 wget $OPTARG -c $URL 
 
-# 3) download transcripts (cDNA) in FASTA format
+## F3) download transcripts (cDNA) in FASTA format
 
 FASTACDNA=${SPECIES}*cdna.all.fa.gz
 URL=${SERVER}/release-${EGRELEASE}/${DIV}/fasta/${SPECIES,,}/cdna/${FASTACDNA}
 echo "# downloading $URL"
 wget $OPTARG -c $URL 
 
-# 4) download soft-masked genomic sequences
+## F4) download soft-masked genomic sequences
 
 FASTASM=${SPECIES}*.dna_sm.toplevel.fa.gz
 URL=${SERVER}/release-${EGRELEASE}/${DIV}/fasta/${SPECIES,,}/dna/${FASTASM}
 echo "# downloading $URL"
 wget $OPTARG -c $URL
 
-# 5) get mappings to UniProt proteins
+## F5) get mappings to UniProt proteins
 
 UNIPTSV=${SPECIES}*.uniprot.tsv.gz
 URL=${SERVER}/${DIV}/release-${EGRELEASE}/tsv/${SPECIES,,}//$UNIPTSV
 echo "# downloading $URL"
 wget $OPTARG -c $URL
 
-# 6) get indexed, bgzipped VCF file with variants mapped,
-# note this contains all variants known to Ensembl Plants,
+## F6) get indexed, bgzipped VCF file with variants mapped
+
+# Note: this contains all variants known to Ensembl Plants,
 # no individual genotypes are conserved
 
 VCF=${SPECIES,,}.vcf.gz*
@@ -67,16 +68,14 @@ URL=${SERVER}/${DIV}/release-${EGRELEASE}/variation/vcf/${SPECIES,,}/${VCF}
 echo "# downloading $URL"
 wget $OPTARG -c $URL
 
-##############################################################
-
-# 7) download all homologies in a single TSV file, several GBs
+## F7) download all homologies in a single TSV file, several GBs
 
 TSVFILE=Compara.${RELEASE}.protein_default.homologies.tsv.gz
 URL=${SERVER}/${DIV}/release-${EGRELEASE}/tsv/ensembl-compara/homologies/${TSVFILE}
 echo "# downloading $URL"
 wget $OPTARG -c $URL
 
-# 8) download UniProt report of Ensembl Plants, 
+## F8) download UniProt report of Ensembl Plants, 
 # summarized how many protein sequences from each species
 # have been annotated in SwissProt & TrEMBL
 
@@ -85,15 +84,16 @@ URL=${SERVER}/${DIV}/release-${EGRELEASE}/$UNIPFILE
 echo "# downloading $URL"
 wget $OPTARG -c $URL
 
-# 9) retrieve list of new species in current release
+## F9) retrieve list of new species in current release
 
 NEWLIST=new_genomes.txt
 URL=${SERVER}/${DIV}/release-${EGRELEASE}/$NEWLIST
 echo "# downloading $URL"
 wget $OPTARG -c $URL
 
-# 10) get current plant species tree (cladogram)
+## F10) get current plant species tree (cladogram)
 TREEFILE=/plants_protein-trees_default.nh
 URL=${SERVER}/${DIV}/release-${EGRELEASE}/compara/species_trees/$TREEFILE
 echo "# downloading $URL"
 wget $OPTARG -c $URL
+

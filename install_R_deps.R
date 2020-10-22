39fafb730c1e58496f0bdc8baca858f16c319fcc
brunocontrerasmoreira <bcontreras@ebi.ac.uk>
2020-10-22 17:00:40 +0100

    added local_lib

--- install_R_deps.R
+++ install_R_deps.R
@@ -10,14 +10,4 @@ if(!requireNamespace("BiocManager", quietly=T))
 
 BiocManager::install("biomaRt", lib=local_lib)
 
-#repository = 'https://cloud.r-project.org';
-#required_packages = c("httr", "jsonlite")
-#for (package in required_packages) {
-#	if (!require(package, character.only=T, quietly=T)) {
-#		sprintf("# cannot load %s, will get it from %s and install it in %s",
-#			package,repository,local_lib)
-#		install.packages(package, dependencies=TRUE, lib=local_lib, repos=repository)
-#	}
-#}
-
 sessionInfo()

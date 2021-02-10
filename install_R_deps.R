71f7dcc0bd0b142b88c578e349d62f0b8b818e3d
brunocontrerasmoreira <bcontreras@ebi.ac.uk>
2021-02-10 09:51:21 +0000

    added httr and jsonlite

--- install_R_deps.R
+++ install_R_deps.R
@@ -8,7 +8,7 @@ local_lib = "./files/R/"
 if(!requireNamespace("BiocManager", quietly=T))
 	install.packages("BiocManager", dependencies=T, lib=local_lib)
 
-install.packages("dplyr", "stringi", "knitr", dependencies=T, lib=local_lib)
+install.packages("dplyr", "stringi", "knitr", "httr", "jsonlite", dependencies=T, lib=local_lib)
 
 BiocManager::install("biomaRt", lib=local_lib, dependencies=T)
 

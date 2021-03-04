c93fc7a6e6a249d35ed333596e153e3d9b85167e
brunocontrerasmoreira <bcontreras@ebi.ac.uk>
2021-03-04 11:49:44 +0000

    added --sift b

--- recipes/exampleVEP.sh
+++ recipes/exampleVEP.sh
@@ -75,7 +75,7 @@ fi
 
 # See more options and examples at 
 # http://www.ensembl.org/info/docs/tools/vep/script/vep_options.html
-# http://www.ensembl.org/info/docs/tools/vep/script/vep_example.html
+# http://www.ensembl.org/info/docs/tools/vep/script/vep_example.html 
 
 VCFILE="${VEPATH}/ensembl-vep/examples/arabidopsis_thaliana.TAIR10.vcf"
 OUTFILE='arabidopsis_thaliana.vep.output'
@@ -89,6 +89,7 @@ VEPOPTIONS=(
 	--check_existing       # co-located known variants
 	--distance 5000        # max dist between variant and transcript
 	--biotype              # show biotype of neighbor transcript
+    --sift b               # note not all species have SIFT precomputed
 	--input_file $VCFILE
 	--output_file $OUTFILE
 )

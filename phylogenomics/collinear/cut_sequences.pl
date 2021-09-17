c8ebe196ec6c8f5762da3c0c2cfc2a5984c6076e
brunocontrerasmoreira <brunocontreras@yahoo.co.uk>
2021-09-17 16:56:23 +0100

    prints mRNAid next to gene id

--- phylogenomics/collinear/cut_sequences.pl
+++ phylogenomics/collinear/cut_sequences.pl
@@ -8,11 +8,11 @@ use Getopt::Long qw(:config no_ignore_case);
 # Uses external software:
 # gffread [https://f1000research.com/articles/9-304/v2]
 
-#perl cut_sequences.pl -sp1 oryza_sativa -fa1 Oryza_sativa.IRGSP-1.0.dna.toplevel.fa -gf1 Oryza_sativa.IRGSP-1.0.51.gff3
+#perl cut_sequences.pl -sp oryza_sativa -fa Oryza_sativa.IRGSP-1.0.dna.toplevel.fa -gf Oryza_sativa.IRGSP-1.0.51.gff3
 
 my $GFFREADEXE = 'gffread'; # v0.12.7
 
-my ( $help, $gffreadpath, $sp1, $fasta1, $gff1 ) = (0);
+my ( $help, $gffreadpath, $sp1, $fasta1, $gff1, $tname ) = (0);
 
 GetOptions(
 	"help|?"       => \$help,
@@ -52,43 +52,81 @@ my $pepfile  = "$sp1.cds.faa";
 
 print "\n# $0 -sp $sp1 -fa $fasta1 -gf $gff1 -path $gffreadpath\n\n";
 
-open(CDNA,">",$cdnafile) || die "# ERROR: cannot create $cdnafile\n";
-open(GFFREAD,"$gffreadpath -w - -g $fasta1 $gff1 |") ||
-	die "# ERROR: cannot run $gffreadpath\n";
-while(<GFFREAD>){
-	if(/^>(\S+)/){
-		print CDNA ">$1 [$sp1]\n"	
+my $ref_genes = parse_genenames($gff1);
+
+my $num_cdna = parse_gffread($gffreadpath,$fasta1,$gff1,$cdnafile,'cdna',$ref_genes);
+my $num_cds  = parse_gffread($gffreadpath,$fasta1,$gff1,$cdsfile,'cds',$ref_genes);
+my $num_pep  = parse_gffread($gffreadpath,$fasta1,$gff1,$pepfile,'pep',$ref_genes);
+
+print "# $cdnafile n=$num_cdna\n";
+print "# $cdsfile n=$num_cds\n";
+print "# $pepfile n=$num_pep\n";
+
+###############################
+
+# Runs gffread, parses its stdout and saves output in FASTA FILE.
+# Returns number of sequences printed out.
+sub parse_gffread {
+
+	my ($gffreadexe,$fasta_file,$gff_file,$outfile,$seqtype,$ref_tr2gene) = @_;
+	my ($params,$mrnaid,$geneid);
+
+	if($seqtype eq 'cds'){
+		$params = '-x - ';
+	} elsif($seqtype eq 'pep'){
+        $params = '-y - ';
 	} else {
-		print CDNA;
+		$params = '-w - '; # cDNA, default
 	}
-}
-close(GFFREAD);
-close(CDNA);
 
-open(CDS,">",$cdsfile) || die "# ERROR: cannot create $cdsfile\n";
-open(GFFREAD,"$gffreadpath -x - -g $fasta1 $gff1 |") ||
-    die "# ERROR: cannot run $gffreadpath\n";
-while(<GFFREAD>){
-    if(/^>(\S+)/){
-        print CDS ">$1 [$sp1]\n"
-    } else {
-        print CDS;
-    }
-}
-close(GFFREAD);
-close(CDS);
+	my $num_seqs = 0;
+	open(OUT,">",$outfile) || die "# ERROR(parse_gffread): cannot create $pepfile\n";
+	open(GFFREAD,"$gffreadexe $params -g $fasta_file $gff_file |") ||
+		die "# ERROR(parse_gffread): cannot run $gffreadexe\n";
+	while(<GFFREAD>){
+    	if(/^>(\S+)/){
+			$mrnaid = $1;
+			$geneid = $ref_tr2gene->{$mrnaid} || '';
 
-open(PEP,">",$pepfile) || die "# ERROR: cannot create $pepfile\n";
-open(GFFREAD,"$gffreadpath -y - -g $fasta1 $gff1 |") ||
-    die "# ERROR: cannot run $gffreadpath\n";
-while(<GFFREAD>){
-    if(/^>(\S+)/){
-        print PEP ">$1 [$sp1]\n"
-    } else {
-        print PEP;
-    }
-}
-close(GFFREAD);
-close(PEP);
+        	print OUT ">$mrnaid $geneid [$sp1]\n";
+			$num_seqs++;
+    	} else {
+        	print OUT;
+    	}
+	}
+	close(GFFREAD);
+	close(OUT);
 
-print "# created $cdnafile $cdsfile $pepfile\n\n";
+	return $num_seqs
+}
+
+# Parses gene names as parent IDs of transcripts.
+# Return ref to hash mapping transcript ID -> gene ID
+sub parse_genenames {
+
+	my ($gff_file) = @_;
+
+	my ($mrnaid,$geneid,%names);
+
+	open(GFF,"<",$gff_file) || die "# ERROR(parse_genenames): cannot read $gff_file\n";
+	while(<GFF>){
+		my @F = split(/\t/,$_);
+
+		next if(scalar(@F)<9 || ($F[2] ne "mRNA" && $F[2] ne "transcript"));
+
+		# take only genes where ID can be parsed
+		if($F[8] =~ /ID=([^;]+).*?Parent=([^;]+)/){ 
+
+			$mrnaid = $1;
+			#$mrnaid =~ s/transcript://; # remove redundant bits
+			$geneid = $2;
+			#$geneid =~ s/gene://; # remove redundant bits
+
+			$names{$mrnaid} = $geneid;
+		}
+	}
+	close(GFF);
+
+	return \%names;
+}
+

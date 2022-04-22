c071cb6761ccc02e26e70d1897e5b3487d3ec253
Bruno Contreras Moreira <contrera@millo.auladei.eead.csic.es>
2022-04-22 11:24:49 +0200

    expects merged TSV file .gz in input folder

--- pangenes/check_evidence.pl
+++ pangenes/check_evidence.pl
@@ -16,7 +16,10 @@ use FindBin '$Bin';
 use lib "$Bin/lib";
 use pangeneTools qw(parse_sequence_FASTA_file);
 
+my $GZIPBIN = $ENV{'EXE_GZIP'} || 'gzip';
+
 my (%opts,$INP_dir,$INP_clusterfile);
+my ($cluster_list_file,$cluster_folder);
 my ($gene_id, %cluster_gene_id, %seen, @pairs);
 
 getopts('hd:i:', \%opts);
@@ -25,21 +28,49 @@ if(($opts{'h'})||(scalar(keys(%opts))==0))
 {
   print "\nusage: $0 [options]\n\n";
   print "-h this message\n";
-  print "-d directory with get_pangenes.pl output   (example: -d /path/to/data_pangenes)\n";
-  print "-i input clusterfile                       (example: -i /path/to/cluster.cdna.fna\n\n";
-  print "Note: reads the merged TSV file from the last run of get_pangenes.pl\n"; 
+  print "-d output directory produced by get_pangenes.pl    (example: -d /path/to/data_pangenes/..._algMmap_)\n";
+  print "-i cluster filename as shown in .cluster_list file (example: -i gene:ONIVA01G52180.cdna.fna)\n\n";
+  print "Note: reads the compressed merged TSV file in -d\n"; 
   exit(0);
 }
 
-if(defined($opts{'d'})){  $INP_dir = $opts{'d'}; }
-else{ die "# EXIT : need a -d directory\n"; }
+if(defined($opts{'d'})){  $INP_dir = $opts{'d'} }
+else{ die "# EXIT : need a -d directory\n" }
 
-if(defined($opts{'i'})){  $INP_clusterfile = $opts{'i'}; }
-else{ die "# EXIT : need parameter -i\n"; }
+if(defined($opts{'i'})){  $INP_clusterfile = $opts{'i'} }
+else{ die "# EXIT : need parameter -i\n" }
 
+# 1) locate .cluster_list file to check clusterfile is there
+opendir(INPDIR,$INP_dir) || 
+  die "# ERROR: cannot list $INP_dir , please check -d argument is a valid folder\n";
+my @files = grep {/\.cluster_list/} readdir(INPDIR);
+closedir(INPDIR);
 
-# 1) parse FASTA headers of input cluster to extract gene names
-my ( $ref_geneid, $ref_fasta, $ref_coords ) = parse_sequence_FASTA_file( $INP_clusterfile );
+if(@files) {
+  $cluster_list_file = $files[0];
+  $cluster_folder = (split(/\.cluster_list/,$cluster_list_file))[0]
+} else {
+  die "# ERROR: cannot find .cluster_list file in $INP_dir\n";
+}
+
+my $clusternameOK = 0;
+open(LIST,"<","$INP_dir/$cluster_list_file") ||
+  die "# ERROR: cannot read $INP_dir/$cluster_list_file, please check -d argument is a valid folder\n";
+
+while(<LIST>) {
+  if(/$INP_clusterfile/) {
+    $clusternameOK = 1;
+  }
+}
+close(LIST);
+
+if($clusternameOK == 0) {
+  die "# ERROR: cannot find $INP_clusterfile in $INP_dir/$cluster_list_file, please correct\n";
+}
+
+# 2) parse FASTA headers of input cluster to extract gene names
+my ( $ref_geneid, $ref_fasta, $ref_coords ) = 
+  parse_sequence_FASTA_file( "$INP_dir/$cluster_folder/$INP_clusterfile" );
 foreach $gene_id (@$ref_geneid) {
   $cluster_gene_id{$gene_id} = 1;
 }
@@ -47,10 +78,10 @@ foreach $gene_id (@$ref_geneid) {
 printf("# cluster genes = %d\n", 
   scalar(keys(%cluster_gene_id)));
 
-# 2) check merged TSV file is in place
-my $mergedTSV = "$INP_dir/tmp/mergedpairs.tsv";
+# 3) parse compressed merged TSV file
+my $mergedTSV = "$INP_dir/mergedpairs.tsv.gz";
 
-open(TSV,"<",$mergedTSV) ||
+open(TSV, "$GZIPBIN -dc $mergedTSV |") ||
   die "# ERROR: cannot find file $mergedTSV, please check -d argument\n";
 
 while(<TSV>) {

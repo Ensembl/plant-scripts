fd822e57e0ee0e1033c7a8b90efe90ae8f80debf
Bruno Contreras Moreira <contrera@millo.auladei.eead.csic.es>
2022-04-22 22:21:06 +0200

    uses DB_File to optimize multiple queries

--- pangenes/check_evidence.pl
+++ pangenes/check_evidence.pl
@@ -2,7 +2,7 @@
 
 # This script takes a cluster produced by get_pangenes.pl and retrieves
 # the collinearity data evidence supporting it, inferred from whole genome
-# alignments
+# alignments, contained in mergedpairs.tsv.gz (sorted with -k1,1 -k4,4nr)
 
 # Copyright [2022]
 # EMBL-European Bioinformatics Institute & Estacion Experimental Aula Dei-CSIC
@@ -12,9 +12,13 @@ $|=1;
 use strict;
 use warnings;
 use Getopt::Std;
+use DB_File;
+use Compress::Zlib qw(compress uncompress);
 use FindBin '$Bin';
 use lib "$Bin/lib";
-use pangeneTools qw(parse_sequence_FASTA_file);
+use pangeneTools qw( parse_sequence_FASTA_file );
+
+$ENV{'LC_ALL'}   = 'POSIX';
 
 my $GZIPBIN = $ENV{'EXE_GZIP'} || 'gzip';
 
@@ -22,7 +26,9 @@ my (%opts,$INP_dir,$INP_clusterfile);
 my ($cluster_list_file,$cluster_folder);
 my ($gene_id, $hom_gene_id, $homology_type, $species, $hom_species);
 my ($overlap, $coords, $hom_coords, $full_id, $hom_full_id);
-my (%seen, %overlap, @pairs, %cluster_gene_id, %fullid2id, %gene_length);
+my ($line, $segment, $hom_segment, $dummy, $TSVdata);
+my (%TSVdb, @sorted_ids, @pairs);
+my (%seen, %overlap, %cluster_gene_id, %fullid2id, %gene_length);
 
 getopts('hd:i:', \%opts);
 
@@ -57,7 +63,8 @@ if(@files) {
 
 my $clusternameOK = 0;
 open(LIST,"<","$INP_dir/$cluster_list_file") ||
-  die "# ERROR: cannot read $INP_dir/$cluster_list_file, please check -d argument is a valid folder\n";
+  die "# ERROR: cannot read $INP_dir/$cluster_list_file, ".
+    "please check -d argument is a valid folder\n";
 
 while(<LIST>) {
   if(/$INP_clusterfile/) {
@@ -74,30 +81,85 @@ if($clusternameOK == 0) {
 my ( $ref_geneid, $ref_fasta, $ref_coords, $ref_taxon ) = 
   parse_sequence_FASTA_file( "$INP_dir/$cluster_folder/$INP_clusterfile" , 1);
 
-foreach $gene_id (@$ref_geneid) {
+foreach $gene_id (sort @$ref_geneid) {
   $cluster_gene_id{$gene_id} = 1;
-}
+  push(@sorted_ids, $gene_id);
+} 
 
-printf("# cluster genes = %d\n", 
+printf("\n# cluster %s genes = %d\n",
+  $INP_clusterfile, 
   scalar(keys(%cluster_gene_id)));
 
-# 3) parse compressed merged TSV file
+
+# 3) parse compressed merged TSV file and feed Berkeley DB
 my $mergedTSV = "$INP_dir/mergedpairs.tsv.gz";
+my $TSVdb_file = "$INP_dir/mergedpairs.tsv.bdb";
 
-open(TSV, "$GZIPBIN -dc $mergedTSV |") ||
-  die "# ERROR: cannot find file $mergedTSV, please check -d argument\n";
+# only first time
+if(!-s $TSVdb_file) {
 
-while(<TSV>) {
-  #gene_stable_id\tprotein_stable_id\tspecies\toverlap\thomology_type\thomology_gene_stable_id\t
-  # homology_protein_stable_id\thomology_species\toverlap\tdn\tds\tgoc_score\twga_coverage\t
-  # is_high_confidence\tcoordinates
+  print "\n# creating database (only first time)\n";
 
-  my @data = split(/\t/,$_);
-  next if(scalar(@data) < 15);
+  tie(%TSVdb, 'DB_File', $TSVdb_file, 
+    O_RDWR|O_CREAT, 0666, $DB_BTREE) ||
+    die "# ERROR: cannot create file $TSVdb_file: $!\n";
 
-  ($gene_id, $species, $overlap, $homology_type, $hom_gene_id, $hom_species, $coords) = 
-    ($data[0],$data[2],$data[3],$data[4],$data[5],$data[7],$data[14]);
+  open(TSV, "$GZIPBIN -dc $mergedTSV |") ||
+    die "# ERROR: cannot find file $mergedTSV, please check -d argument\n";
 
+  my ($block, $prev_gene_id) = ('', '');
+  while(<TSV>) {
+    if(/^([^\t]+)/) {
+      $gene_id = $1;
+
+      if($gene_id ne $prev_gene_id) {
+
+        if($block) {
+          $TSVdb{$prev_gene_id} = compress($block);
+        }
+        
+        $block = $_;
+        $prev_gene_id = $gene_id;
+
+      } else {
+        $block .= $_;
+      }
+    }
+  }   
+
+  $TSVdb{$gene_id} = compress($block);
+
+  close(TSV);
+
+  print "# done\n";  
+
+} else {
+
+  print "# re-using database\n";
+
+  tie(%TSVdb, 'DB_File', $TSVdb_file,
+    O_RDWR, 0666, $DB_BTREE) ||
+    die "# ERROR: cannot read file $TSVdb_file: $!\n";
+}
+
+# 4) parse TSV blocks for selected genes  
+foreach $gene_id (@sorted_ids) {
+
+  next if(!$TSVdb{$gene_id});
+
+  $TSVdata = uncompress($TSVdb{$gene_id});
+ 
+foreach $line (split(/\n/,$TSVdata)) { 
+
+  #LOC_Os01g01019 LOC_Os01g01019 oryza_sativa_MSU 1065 ortholog_collinear
+  #gene:Osir64_01g0000020 gene:Osir64_01g0000020 oryza_sativa_ir64 1065	
+  #NULL NULL NULL 100.00 1 Chr1:11217-12435(+);1:19441-20506(+)
+
+  ( $gene_id, $segment, $species, $overlap, $homology_type, 
+    $hom_gene_id, $hom_segment, $hom_species, $dummy, 
+    $dummy, $dummy, $dummy, $dummy, $dummy, $coords   
+  ) = split( /\t/, $line );
+ 
   if($homology_type eq 'ortholog_collinear') {
 
     next if(!$cluster_gene_id{$gene_id} || !$cluster_gene_id{$hom_gene_id});
@@ -117,13 +179,13 @@ while(<TSV>) {
     $overlap{$hom_full_id} += $overlap; 
 
     # compute gene length
-    ($coords, $hom_coords) = split(/;/,$coords);
+    ($coords, $hom_coords) = split(/;/,$coords); 
 
     if(!$gene_length{$full_id}) {
       if($coords =~ m/^\S+?:(\d+)-(\d+)\([+-]\)/) {
         $gene_length{$full_id} = 1+$2-$1;
       } else {
-        die "# ERROR: cannot parse $coords\n";
+        die "# ERROR: cannot parse $coords $_\n";
       }
     }
 
@@ -135,7 +197,7 @@ while(<TSV>) {
       }
     }
 
-    push(@pairs, $_);
+    push(@pairs, "$line\n");
 
   } elsif($homology_type eq 'segment_collinear') {
 
@@ -144,26 +206,29 @@ while(<TSV>) {
     next if($cluster_gene_id{$hom_gene_id} && $ref_taxon->{$hom_gene_id} ne $hom_species);
 
     # segment-gene pairs not considered for stats 
-    #if($data[1] ne 'segment'){ $seen{$gene_id}++ }
-    #elsif($data[6] ne 'segment'){ $seen{$hom_gene_id}++ }
 
-    push(@pairs, $_)
+    push(@pairs, "$line\n");
   }
 
 }
-close(TSV);
 
+}
+
+untie(%TSVdb);
+
+# 5) print raw collinear TSV evidence
 if(scalar(keys(%seen)) != scalar(keys(%cluster_gene_id))) {
   die "# ERROR: cannot find collinear evidence for cluster $INP_clusterfile ,".
     "please re-run get_pangenes.pl with same arguments\n";
 }
 else { 
-  print "\n#gene_stable_id\tprotein_stable_id\tspecies\toverlap\thomology_type\thomology_gene_stable_id\t" .
-    "homology_protein_stable_id\thomology_species\toverlap\tdn\tds\tgoc_score\twga_coverage\t" .
-    "is_high_confidence\tcoordinates\n";
+  print "\n#gene_stable_id\tprotein_stable_id\tspecies\toverlap\thomology_type\t" .
+    "homology_gene_stable_id\thomology_protein_stable_id\thomology_species\t".
+    "overlap\tdn\tds\tgoc_score\twga_coverage\tis_high_confidence\tcoordinates\n";
   print @pairs;
 }
 
+# 6) print summary stats
 print "\n#gene\tlength\tpairs\tgene_overlap\tspecies\n";
 foreach $full_id (sort {$seen{$b} <=> $seen{$a}} (keys(%seen))){
 

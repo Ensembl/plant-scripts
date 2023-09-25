334c985286fa855774cd051062a80f5545902b0e
eead-csic-compbio <bcontreras@eead.csic.es>
2023-09-25 17:18:57 +0200

    1st complete version, writes TSV to -o file

--- pangenes/match_cluster.pl
+++ pangenes/match_cluster.pl
@@ -1,8 +1,8 @@
 #!/usr/bin/env perl
 
-# This script matches input sequences to clusters produced by get_pangenes.pl
+# This script matches input nucleotide sequences to clusters produced by get_pangenes.pl .
 # It creates a sequence index with nucleotide sequences from clusters and 
-# uses GMAP tp scan them. Can use cdna [default ]or cds sequences.
+# uses GMAP to scan them. Can use cdna [default ] or CDS sequences.
 
 # Copyright [2023]
 # EMBL-European Bioinformatics Institute & Estacion Experimental Aula Dei-CSIC
@@ -27,11 +27,14 @@ my $GMAPBIN = $ENV{'EXE_GMAP'};
 my $GMAPBUILDBIN = $ENV{'EXE_GMAP_BUILD'};
 
 my ($INP_dir, $INP_seqfile, $INP_isCDS, $INP_idfile) = ('','',0,'');
+my ($INP_outfile,$INP_threads, $INP_ow, $INP_verbose) = ('',4, 0, 0);
 my ($cluster_regex, $isCDS, $gmapdb_path, $gmapdb, $seq) = ( '.cdna.fna', 0 );
+my ($max_number_isoforms, $qlen, $tlen, $identity, $cover) = ( 0 );
 my ($cluster_file, $gene_id, $isof_id, $cmd, $index_file);
-my (%opts);
+my ($cluster_id, $seq_id, $cluster_seq_id, $coords, $taxon);
+my (%opts, %matches, @order_geneid);
 
-getopts('hcCI:d:s:', \%opts);
+getopts('hvcCwI:d:s:t:o:', \%opts);
 
 if(($opts{'h'})||(scalar(keys(%opts))==0))
 {
@@ -39,9 +42,14 @@ if(($opts{'h'})||(scalar(keys(%opts))==0))
   print "-h this message\n";
   print "-c print credits and checks installation\n";
   print "-d directory with pangene clusters.pl             (required, should contain *.cdna.fna or *.cds.fna files)\n";
-  print "-s nucleotide sequence file in FASTA format       (required, example: -s transcripts.fna)\n";
+  print "-s nucleotide sequence file in FASTA format       (required, example: -s transcripts.fna,\n";
+  print "                                                   useful to have genomic coords in header ie chr1:12-1200)\n";
+  print "-o output file in TSV format                      (required)\n";
   print "-C use CDS sequences                              (optional, cDNA sequences are scanned by default)\n";
-  print "-I TSV file matching cluster names to pangene ids (optional, example: -I cluster2id.tsv)\n\n";
+  print "-t threads                                        (optional, default: -t $INP_threads)\n";
+  print "-w overwrite GMAP index                           (optional, by default index is re-used if possible)\n";
+  print "-v verbose                                        (optional)\n";
+  #print "-I TSV file matching cluster names to pangene ids (optional, example: -I cluster2id.tsv)\n\n";
   exit(0);
 }
 
@@ -64,8 +72,18 @@ else{ die "# EXIT : need a -d directory\n" }
 
 if(defined($opts{'s'})){  
   $INP_seqfile = $opts{'s'};
+
+  if(!-e $INP_seqfile) {
+    die "# EXIT : need a valid -s file\n"
+  }
 } 
 else{ die "# EXIT : need parameter -s\n" }
+
+if(defined($opts{'o'})){
+  $INP_outfile = $opts{'o'};
+} 
+else{ die "# EXIT : need parameter -o\n" }
+
 
 if(defined($opts{'C'})){
   $INP_isCDS = 1;
@@ -76,7 +94,22 @@ if(defined($opts{'I'})){
   $INP_idfile = $opts{'I'};  
 }
 
-# 1) parse cluster files and write sequences to temp file
+if(defined($opts{'w'})){
+  $INP_ow = 1;
+}
+
+if(defined($opts{'v'})){
+  $INP_verbose = 1;
+}
+
+if(defined($opts{'t'}) && $opts{'t'} >= 1){
+  $INP_threads = int($opts{'t'});
+}
+
+print "# $0 -d $INP_dir -s $INP_seqfile -o $INP_outfile -C $INP_isCDS " .
+  "-w $INP_ow -t $INP_threads -v $INP_verbose\n\n";
+
+# 1) parse pre-computed cluster files, write sequences to temp file & make GMAP index
 opendir(INPDIR,$INP_dir) || 
   die "# ERROR: cannot list $INP_dir , please check -d argument is a valid folder\n";
 my @files = grep {/$cluster_regex$/} readdir(INPDIR);
@@ -86,135 +119,137 @@ if(scalar(@files) == 0) {
   die "# ERROR: cannot find any valid clusters in $INP_dir\n";
 }
 
-my ($fh, $filename) = tempfile( 'tempgmapfnaXXXXX', UNLINK => 1);
-
-foreach $cluster_file (@files) {
-
-  my ( $ref_geneid, $ref_fasta, $ref_isof_coords, $ref_taxon ) = 
-    parse_sequence_FASTA_file( "$INP_dir/$cluster_file" , 1);
-
-  # prepend cluster name and print this sequence 
-  foreach $gene_id (@$ref_geneid) {
-    $seq = $ref_fasta->{$gene_id};
-    $seq =~ s/^>/>$cluster_file-/;   
-    print $fh $seq;
-  }
-}
-
-## 2) index temp sequences for GMAP, skip if done previously
-$cmd = "$GMAPBUILDBIN -d $gmapdb -D $gmapdb_path $filename 2>&1";
+# check whether previous index should be re-used
 $index_file = "$gmapdb.ref153positions";
 
-if(!-d "$gmapdb_path/$gmapdb" || 
+if($INP_ow ||
+  !-d "$gmapdb_path/$gmapdb" ||
   !-e "$gmapdb_path/$gmapdb/$index_file") {
+
+  my ($fh, $filename) = tempfile( '/tmp/tempfnaXXXXX', UNLINK => 1);
+
+  foreach $cluster_file (@files) {
+
+    my ( $ref_geneid, $ref_fasta, $ref_isof_coords, $ref_taxon ) = 
+      parse_sequence_FASTA_file( "$INP_dir/$cluster_file" , 0);
+
+    foreach $gene_id (@$ref_geneid) {  
+      foreach $seq (split(/\n/,$ref_fasta->{$gene_id})) {
+
+        if($seq =~ /^>(\S+) (\S+) (\S+) (\S+)/) {
+          # FASTA headers in clusters produced by get_pangenes.pl look like this:
+          #>transcript:Os01t0100100-01 gene:Os01g0100100 1:2983-10815(+) [Oryza_sativa.IRGSP-1.0.chr1]
+
+          # prepend cluster filename to header and print to temp file
+          ($isof_id, $gene_id, $coords, $taxon) = ($1, $2, $3, $4);
+          print $fh ">$cluster_file|$isof_id|$gene_id|$coords|$taxon\n";
+
+        } else {
+          print $fh "$seq\n";
+        }
+      }
+    }
+  } 
+
+
+  # actually create GMAP index from temp file
+  $cmd = "$GMAPBUILDBIN -e 0 -d $gmapdb -D $gmapdb_path $filename 2>&1";
 
   print "# building GMAP index $gmapdb_path/$gmapdb\n\n";
   system($cmd);
   if ( $? != 0 ) {
     die "# ERROR: failed running $GMAPBUILDBIN ($cmd)\n";
   } else {
-    print "# created GMAP index $gmapdb_path/$gmapdb\n\n";
+    print "\n# created GMAP index $gmapdb_path/$gmapdb\n\n";
   }
+
 } else {
-  print "# re-using GMAP index $gmapdb_path/$gmapdb\n\n";
+  print "\n# re-using GMAP index $gmapdb_path/$gmapdb\n\n";
 }
 
-## 3) map input sequences to cluster sequences
-$cmd = "$GMAPBIN -S -d $gmapdb -D $gmapdb_path $INP_seqfile 2>&1";
+## 2) map input sequence(s) to indexed cluster sequences with GMAP
+$cmd = "$GMAPBIN --no-chimeras -t $INP_threads -n 100 -S -d $gmapdb -D $gmapdb_path $INP_seqfile 2>&1";
 open(GMAP, "$cmd |") ||
   die "# ERROR: cannot run $cmd\n"; 
 while(<GMAP>) { 
-	#if(/^\s+Percent identity: (\S+) \((\d+) matches, (\d+) mismatches, (\d+) indels/){ 
-	#($identity, $match, $mismatch, $indel) = ($1, $2, $3, $4); 
-	#if($identity < $min_identity) {
-	#($match, $mismatch, $indel) = (0, 0, 0);
-	#}
-	#}
 
-    # fails is there are large indels
-    #elsif(/^aa\.([cg])\s+\d+\s(.*)/) {
-    #  #aa.g        48  N  N  P  S  S  Q  I  T  Y  G  L  T  I  H  H  A  V 
-    #  ($seqname,$aa) = ($1,$2);
-    #  $aaseq{$seqname} .= $aa }
-    print;
+  print if($INP_verbose);
+  chomp;	
+
+  #>transcript:Os01t0147300-01 gene:Os01g0147300 1:2568051-2570029(+) [Oryza_sativa.IRGSP-1.0.chr1]
+  if(/^>(.*)/){ 
+    # input FASTA headers could be in any format 
+    $seq_id = $1;
+    push(@order_geneid, $seq_id);
+
+  } elsif(/^  Path \d+: .*?(\d+) bp\)$/){
+    #Path 1: query 1..1768 (1768 bp) => genome gene:...:1..1,820 (1820 bp)
+    ($tlen) = ($1); 
+
+  } elsif(/^    Coverage: (\S+) \(query length: (\d+) bp/){	  
+    ($cover,$qlen) = ($1, $2);
+
+  } elsif(/^    Percent identity: (\S+)/) {
+    #Percent identity: 87.4 (546 matches, 62 mismatches, 17 indels, 0 unknowns)
+    ($identity) = ($1);
+
+  } elsif(/^    \+(\S+)\s+\(\d+-\d+\)/){
+    # expected to be on the plus/+ strand as these are sequences cut from GFF in the right strand:
+    # +cluster.cdna.fna|Os01t0829900-01|gene:Os01g0829900|1:35501996-35505052(-)|[taxon]:1-1820  (1-1768)   97%
+
+    ($cluster_seq_id) = ($1);
+    if($cluster_seq_id =~ m/^([^\|]+)\|([^\|]+)\|([^\|]+)\|([^\|]+)\|\[([^\]]+)\]:/){
+      ($cluster_id, $isof_id, $gene_id, $coords, $taxon) = ($1, $2, $3, $4, $5);
+    }
+
+    # compile match stats
+    $matches{$seq_id}{$cluster_id}{'total'}++;
+
+    if(!defined($matches{$seq_id}{$cluster_id}{'cover'}) || 
+      $cover > $matches{$seq_id}{$cluster_id}{'cover'}) {
+      $matches{$seq_id}{$cluster_id}{'cover'} = $cover;
+      $matches{$seq_id}{$cluster_id}{'identity'} = $identity;
+      $matches{$seq_id}{$cluster_id}{'coords'} = $coords;
+      $matches{$seq_id}{$cluster_id}{'qlength'} = $qlen;
+      $matches{$seq_id}{$cluster_id}{'tlength'} = $tlen;
+    }
+  }
 }
 close(GMAP);
 
 
+## 3) print results table
 
+open(TSV,">",$INP_outfile) ||
+  die "# ERROR: cannot create $INP_outfile\n";
 
+print TSV "#query\tqlength\tpangene\tlength\t".
+  "matches\tperc_qcover\tperc_identity\tcoords\n";
 
-#foreach $gene_id (@$ref_geneid) {
-#  foreach $isof_id (keys(%{$isof_len{$gene_id}})) {
+foreach $seq_id (@order_geneid) {
 
-#    $taxa{ $ref_taxon->{$gene_id} }++;
+  # no matches
+  if(!defined($matches{$seq_id})) {
+      print TSV "$seq_id\tNA\tNA\tNA\tNA\tNA\tNA\tNA\n";
+      next;
+  }
+	
+  # note there might be 1+ rows per input sequence, 
+  # a sequence can potentially match several clusters
+  foreach $cluster_id (keys(%{ $matches{$seq_id} })) {
 
-    # find GFF file & get number of exons
-    #$n_exons = 0;
-    #$gff_file = $updir . "/_$ref_taxon->{$gene_id}.gff";
-    #open(GREP, "$ENV{'EXE_GREP'} '$isof_id;' $gff_file |");
-    #while(<GREP>) {
-    #  #1	NAM	exon	2575663	2575953 ...
-    #  my @data = split(/\t/,$_);
+    printf(TSV "%s\t%d\t%s\t%d\t%d\t%d\t%1.1f\t%s\n",
+      $seq_id,
+      $matches{$seq_id}{$cluster_id}{'qlength'},
+      $cluster_id,
+      $matches{$seq_id}{$cluster_id}{'tlength'},
+      $matches{$seq_id}{$cluster_id}{'total'},      
+      $matches{$seq_id}{$cluster_id}{'cover'},
+      $matches{$seq_id}{$cluster_id}{'identity'},
+      $matches{$seq_id}{$cluster_id}{'coords'},
 
-    #  if($isCDS == 0 && $data[2] eq 'exon') {
-    #    $n_exons++
+    );
+  }
+}
 
-    #  } elsif($isCDS == 1 && $data[2] eq 'CDS') {
-    #    $n_exons++
-    #  }
-    #}
-    #close(GREP);  
-    #push(@exons, $n_exons);
-
-    # actually print to temp file
-    #print $fh "$isof_header{$gene_id}{$isof_id}\n$isof_seq{$gene_id}{$isof_id}\n";
-    #  }
-    #}
-
-#$occup = scalar(keys(%taxa));
-#$n_isof = scalar(@len); # recompute in case inly 1st isoform taken
-
-
-# 4) compute multiple sequence alignment (MSA), distance matrix & MSA report
-
-#if($INP_outdir ne '') {
-#  $msa_filename = "$INP_outdir/$INP_clusterfile";
-#  $msa_filename =~ s/\.(f[na]a)$/.aln.$1/;
-#  $dist_filename = "$INP_outdir/$INP_clusterfile";
-#  $dist_filename =~ s/\.(f[na]a)$/.dist.$1/;
-#} else {
-#  ($fhmsa, $msa_filename) = tempfile( 'tempmsaXXXXX', UNLINK => 1);
-#  ($fhdist, $dist_filename) = tempfile( 'tempdistXXXXX', UNLINK => 1);
-#}
-
-#$cmd = "$ENV{'EXE_CLUSTALO'} --force --full -i $filename -o $msa_filename --distmat-out=$dist_filename 2>&1";
-#system($cmd); 
-#if ( $? != 0 ) {
-#  die "# ERROR: failed running clustal-omega ($cmd)\n";
-#} elsif ( !-s $msa_filename ) {
-#  die "# ERROR: failed generating $msa_filename file ($cmd)\n";
-#}
-
-# parse MSA distances
-#$max_dist = -1;
-#open(DIST,"<",$dist_filename) ||
-#  die "# ERROR: cannot read $dist_filename\n";
-#while(<DIST>) {
-#  chomp;
-#  my @data = split(/\s+/,$_);
-#  next if($#data < 1);
-#  foreach $c (1 .. $#data) { 
-#    push(@dist, $data[$c]);
-#    if($data[$c] > $max_dist){ $max_dist = $data[$c] } 
-#  }  
-#}
-#close(DIST);
-
-
-# MSA report
-#$cmd = "$ENV{'EXE_ALISTAT'} $msa_filename 1 -b";
-#if($ispep) {
-#  $cmd = "$ENV{'EXE_ALISTAT'} $msa_filename 6 -b";
-#}
-
+close(TSV);

424f2f354b7acb044013185e567e8c714d4ff505
brunocontrerasmoreira <brunocontreras@yahoo.co.uk>
2024-10-04 16:18:26 +0100

    work in progress, -r already computes identical clusters

--- pangenes/rename_pangenes.pl
+++ pangenes/rename_pangenes.pl
@@ -1,7 +1,8 @@
 #!/usr/bin/env perl
 
 # This script assigns stable IDs to clusters and transposed pangene matrices made by get_pangenes.pl
-# 
+# Dependencies: uses system sort & join
+#
 # Inspired by proposal previously discussed in AgBioData group:
 # [clade].[group].[version].panddddddd
 # where:
@@ -14,7 +15,12 @@
 # 0000001 onwards for pan-gene clusters with 2 or more members
 # 1000001 for singletons (genes found in only one genome)
 #
-# TODO: If a reference set of pangenes is passed, these will guide the nomenclature, need rules
+# If a reference set of pangenes is passed, these will guide the nomenclature, current rules 
+# to go from version n to version n+1:
+# * if reference gene membership is unchanged and >=50% of other existing members stay in the same cluster, the same identifier as before is used
+# * if cluster did not contain a reference gene in version n, and does not in version n+1, then >=50% members rule is maintained
+# * singletons get assigned the codes starting at 1000001 so they don’t interfere with numbering system of others
+# * If a reference gene starts as a singleton, but later clusters with other genes, then it will be assigned a non-singleton (0000001 type) identifier
 
 # Copyright [2023-24]
 # EMBL-European Bioinformatics Institute & Estacion Experimental Aula Dei-CSIC
@@ -26,15 +32,24 @@ use warnings;
 use Getopt::Std;
 use File::Basename;
 use File::Copy;
+use File::Temp qw/ tempfile /;
 use FindBin '$Bin';
 use lib "$Bin/lib";
-use pangeneTools qw( parse_sequence_FASTA_file );
+use pangeneTools;
+
+my $JOINBIN      = $ENV{'EXE_JOIN'} || 'join';
+my $SORTBIN      = $ENV{'EXE_SORT'} || 'sort';
+my $SORTPARS     = "--buffer-size=1G ";
+$ENV{'LC_ALL'}   = 'POSIX';
+
 
 my $GENEIDIGITS = 6;
 
 my ($INP_dir, $INP_refdir, $INP_outdir, $INP_verbose) = ('','','',1);
-my ($cluster_folder, $cluster_folder_name, $cluster_list_file ) = ( '','','' );
-my (%opts, @matrix_files);
+my ($cluster_folder, $cluster_folder_name, $cluster_list_file, $matfile) = ('','','','');
+my ($ref_cluster_folder, $ref_cluster_folder_name, $ref_cluster_list_file, $ref_matfile) = ('','','','');
+my ($f,$colr,$col,$row,$rowr,$cmd);
+my (%opts, @matrix_files, @ref_matrix_files);
 
 getopts('hScd:r:o:', \%opts);
 
@@ -43,10 +58,10 @@ if(($opts{'h'})||(scalar(keys(%opts))==0))
   print "\nusage: $0 [options]\n\n";
   print "-h this message\n";
   print "-c print credits and checks installation\n";
-  print "-d input directory with get_pangenes.pl results (required, example: -d /path/data_pangenes/..._algMmap_)\n";
-  print "-o output directory and prefix ID               (required, example: -o Os4530.POR.1)\n";
-#  print "-r directory with reference pangenes            (optional, these help renaming the input ones)\n";
-  print "-S silent                                       (optional, by default prints mappings old -> renamed)\n";
+  print "-d input directory with get_pangenes.pl results    (required, example: -d /path/data_pangenes/..._algMmap_)\n";
+  print "-o output directory for pangene set and prefix ID  (required, example: -o Os4530.POR.1)\n";
+  print "-r folder with previous reference pangene set      (optional, expects common taxa)\n";
+  print "-S silent                                          (optional, by default prints mappings old -> renamed)\n";
   print 
   exit(0);
 }
@@ -109,8 +124,14 @@ closedir(INPDIR);
 
 if(!@matrix_files) {
   die "# ERROR: cannot locate pangene matrices within $INP_dir\n";
+} else {
+  foreach $f (@matrix_files) {
+    if($f eq 'pangene_matrix_genes.tr.tab') {
+      $matfile = $INP_dir . '/' . $f;
+	  last; 
+    }
+  }
 }
-
 
 
 ## 2) create output directory
@@ -125,7 +146,6 @@ if ( -e $INP_outdir ) {
 }
 
 ## 3) parse cluster list file, rename clusters and create copies in output folder
-
 my ($clustname,$size,$taxa,$gtaxa,$cdnafile,$cdsfile,$pepfile,$gdnafile);
 my ($file, $newfile, $namecol);
 my ($new_clustname, $clust_num, $clust_num1) = ('', 0, 0);	
@@ -247,12 +267,153 @@ if(!$INP_refdir) {
     close(NEWMATFILE);   
   } 
 
-} else {
+} else { # 3.2 reference clusters passed to guide the nomenclature of new ones
 
-  # 3.2
+  my (@ref_taxa, @ref_clusters, @ref_cluster_names);
+  my (@taxa, @clusters, @cluster_names, %input2ref, %input2ref_cluster);
+  my ($n_common_taxa, $n_ref_clusters, $n_identical_clusters) = (0,0,0);
 
-  # TODO, steps:
-  # i) read ref cluster list
-  # ii) read input cluster list
-  # iii) match clusters ii -> i and decide appropriate names case-by-case
+  ## 3.2.0) locate .cluster_list file and guess actual folder with reference clusters
+  opendir(REFDIR,$INP_refdir) ||
+    die "# ERROR: cannot list $INP_refdir , please check -r argument is a valid folder made with this script\n";
+  my @files = grep {/\.cluster_list/} readdir(REFDIR);
+  closedir(REFDIR);
+
+  if(@files) {
+    $ref_cluster_list_file = $files[0];
+    $ref_cluster_folder_name = (split(/\.cluster_list/,$cluster_list_file))[0];
+
+    $ref_cluster_list_file = $INP_refdir . '/' . $cluster_list_file;
+    $ref_cluster_folder = $INP_refdir . '/' . $cluster_folder_name;
+
+  } else {
+    die "# ERROR: cannot locate folder with clusters within $INP_refdir\n";
+  }
+
+  ## 3.2.1) locate matrix files (transposed only .tr) 
+  opendir(REFDIR,$INP_refdir) ||
+    die "# ERROR: cannot list $INP_refdir , please check -r argument is a valid folder made with this script\n";
+  @ref_matrix_files = grep {/pangene_matrix_genes.tr.tab/} readdir(REFDIR);
+  closedir(REFDIR);
+
+  if(!@ref_matrix_files) {
+    die "# ERROR: cannot locate pangene matrices within $INP_refdir\n";
+  } else {
+    $ref_matfile = $INP_refdir . '/' . $ref_matrix_files[0];
+  }
+
+  # 3.2.2) read reference pangene_matrix_genes file 
+  open(GENEMATR,"<",$ref_matfile) ||
+    die "# ERROR: cannot read $ref_matfile\n";
+  while(<GENEMATR>) {  
+    #source:.. taxon1 taxon2 .. taxonN
+    #chr:unsorted    NA      NA      ..      
+    #Os4530.POR.1.pan0000001 Os01g0115300,LOC_Os01g02530     -       ..
+    chomp;
+    my @data = split(/\t/, $_);
+    if($data[0] =~ /^source:/) {		
+      @ref_taxa = @data[1 .. $#data];
+
+	} elsif($data[0] =~ /^chr:/) { 
+
+    } else {
+      push(@ref_cluster_names, shift(@data));
+      push(@ref_clusters, \@data);	   
+      $n_ref_clusters++;
+    }
+  }
+  close(GENEMATR);
+
+  # 3.2.3) read input pangene_matrix_genes file
+  open(GENEMAT,"<",$matfile) ||
+    die "# ERROR: cannot read $matfile\n";
+  while(<GENEMAT>) {
+    #source:.. taxon1 taxon2 .. taxonN
+    #chr:unsorted    NA      NA      ..      
+    #Os4530.POR.1.pan0000001 Os01g0115300,LOC_Os01g02530     -       ..
+    chomp;
+    my @data = split(/\t/, $_);
+    if($data[0] =~ /^source:/) {
+      @taxa = @data[1 .. $#data];
+
+	  # check common taxa
+	  foreach $col (0 .. $#taxa) {
+        foreach $colr (0 .. $#ref_taxa) {
+          next if(defined($input2ref{$colr})); 			
+          if($ref_taxa[$colr] eq $taxa[$col]) {
+            $input2ref{$colr} = $col; #print "$colr $col $ref_taxa[$colr] $taxa[$col]\n";
+            $n_common_taxa++;			
+            last;		 	
+		  }
+        }		  
+      }
+      if($n_common_taxa == 0) {
+        die "# ERROR: no common taxa between $matfile and $ref_matfile, exit\n";
+      }
+
+    } elsif($data[0] =~ /^chr:/) {
+
+    } else {
+      push(@cluster_names, shift(@data));
+
+      # save matrix row in same order as reference matrix
+      my @row;	   
+	  foreach $colr (0 .. $#ref_taxa) {
+        push(@row,$data[$input2ref{$colr}]);
+      }		  
+      push(@clusters, \@row);
+    }
+  }
+  close(GENEMAT);
+
+  # 3.2.4) match input to reference clusters (sort & join)
+  my ($infh, $tempmatfile) = tempfile(UNLINK => 1);
+  my ($infhr, $tempmatfiler) = tempfile(UNLINK => 1);
+  my ($infhs, $tempmatfiles) = tempfile(UNLINK => 1);
+  my ($infhrs, $tempmatfilers) = tempfile(UNLINK => 1);
+
+  foreach $row (0 .. $#clusters) { 
+    print $infh join(':',@{$clusters[$row]})."\t$cluster_names[$row]\n" 
+  }
+
+  foreach $rowr (0 .. $#ref_clusters) { 
+    print $infhr join(':',@{$ref_clusters[$rowr]})."\t$ref_cluster_names[$rowr]\n" 
+  }
+
+  system("$SORTBIN $tempmatfile > $tempmatfiles");
+  system("$SORTBIN $tempmatfiler > $tempmatfilers");
+
+  $cmd = "$JOINBIN -o 1.2,2.2 -a 1 $tempmatfiles $tempmatfilers"; # > $tempjoinfile";
+
+  open(SYSJOIN,"$cmd |") ||
+    die "# ERROR: cannot run $cmd\n";
+  while(<SYSJOIN>) {
+    #  gene:OsGoSa_12g0016870 Os4530.POR.1.pan0040985
+    #  OsMH63_12G017040 
+    if(/^(\S+)\s+(\S+)/){ 
+      $input2ref_cluster{ $1 } = $2;
+	  $n_identical_clusters++;
+    } elsif(/^(\S+)/){
+      $input2ref_cluster{ $1 } = 'NA';
+    }		
+  }
+  close(SYSJOIN);  
+
+
+  # 3.2.5) match input to reference clusters and decide appropriate names, case-by-case
+  #my %seen;
+  #foreach $row (0 .. $#clusters) {
+  #  foreach $rowr (0 .. $#ref_clusters) {
+  #    next if(defined($seen{$rowr}));		
+  #    #print join("\t",@{$clusters[$row]})."\n";		
+  #    if(join("\t",@{$clusters[$row]}) eq join("\t",@{$ref_clusters[$rowr]})) {
+  #      #print "$cluster_names[$row] $ref_cluster_names[$rowr]\n";
+#		$n_identical_clusters++;
+   #     $seen{$rowr}++;		
+  #      last;		
+  #    }
+  #  }
+  #}
+  
+  print "# identical clusters: $n_identical_clusters / $n_ref_clusters\n";
 }
